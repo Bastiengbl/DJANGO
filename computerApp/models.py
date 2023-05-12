f69bdab0d3f6ff63ffd935d3ff2a29290b8d5249
Bastien Gibel <bastien.gibel@etu.univ-poitiers.fr>
2023-05-12 14:50:10 +0200

    test

---
+++ computerApp/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from datetime import datetime
+
+class Machine(models.Model):
+    TYPE = (
+        ('PC', ('PC - Run Windows')),
+        ('Mac', ('%AC - Run MacOS')),
+        ('Serveur', ('Serveur - Simple Server toi deploy virtual machines')),
+        ('Switch', ('Switch - To maintain and connect servers')),
+            )
+
+    id = models.AutoField(primary_key= True, editable=False)
+    nom = models.CharField(max_length=6)
+    maintenanceDate = models.DateField(default = datetime.now())
+    mach = models.CharField(max_length=32, choices=TYPE, default='PC')
+    IP = models.GenericIPAddressField(protocol='both', default='192.168.0.1')
+
+    def __str__(self):
+        return str(self.id) + " --> " + self.nom + " " + self.IP
+
+class Personnel(models.Model):
+
+    TYPE = (
+        ('Developer', ('Developer - Member of the dev team')),
+        ('Assistant', ('Assistant - Helps the dev team')),
+        ('Software_Engineer', ('Software_Engineer - designs and develops effective softwares solutions')),
+        ('Team_Leader', ('Team_Leader - guides and leads a group of devs')),
+
+    )
+    socialsecurity = models.CharField(primary_key=True, max_length=15, editable=True)
+    nom = models.CharField(max_length=20)
+    prenom = models.CharField(max_length=20)
+
+    def __str__(self):
+        return str(self.socialsecurity) + " -->" + self.nom + " " + self.prenom
---
+++ computerApp/views.py
@@ -0,0 +1,51 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from computerApp.models import Machine, Personnel
+from .forms import AddMachineForm
+
+def index(request) :
+    #on recupere l'ensemble des machines de la database
+    machines = Machine.objects.all()
+    
+    #que l'on passe en parametre de la page html via la syntaxe suivante
+    context = {
+        'machines' : machines,
+    }
+
+    return render(request, 'index.html', context)
+
+def machine_list_view(request) :
+    machines = Machine.objects.all()
+    context={'machines': machines}
+    return render(request, 'computerApp/machine_list.html',
+    context)
+
+def personne_list_view(request) : 
+    personnes = Personnel.objects.all()
+    context={'personnes': personnes}
+    return render(request, 'computerApp/personne_list.html',
+    context)
+
+def machine_detail_view(request, pk):
+    machine = get_object_or_404(Machine, id=pk)
+    context={'machine': machine}
+    return render(request,
+    'computerApp/machine_detail.html', context)
+
+def personne_detail_view(request, pk):
+    personne = get_object_or_404(Personnel, socialsecurity=pk)
+    context={'personne': personne}
+    return render(request, 'computerApp/personne_detail.html', context)
+
+#formulaire
+
+def machine_add_form(request):
+    if request.method == 'POST':
+        form = AddMachineForm(request.POST or None)
+        if form.is_valid():
+            new_machine = Machine(nom=form.cleaned_data['nom'])
+            new_machine.save()
+            return redirect('machines')
+    else:
+        form = AddMachineForm()
+        context = {'form': form}
+        return render(request, 'computerApp/machine_add.html', context)
